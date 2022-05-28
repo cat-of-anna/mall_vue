f024de2e443d2b8fa4a6fe0e62b56596c104506a
gezixi <1149994515@qq.com>
2022-05-29 01:52:16 +0800

    feature:跨域配置，删除无关components

--- mall_vue/src/views/Home.vue
+++ mall_vue/src/views/Home.vue
@@ -1,26 +1,14 @@
 <template>
   <div>
-    <h1>商城首页</h1>
-    <el-row class="mb-4">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-      <el-button>中文</el-button>
-      <el-rate v-model="store.value2" :colors="store.colors"></el-rate>
-    </el-row>
+    <Header></Header>
+
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup>
-import {reactive} from "vue";
-
-const store = reactive({
-  value2: null,
-  colors: ['yellow', 'skyblue', 'green']
-})
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <style scoped>
--- mall_vue/vite.config.js
+++ mall_vue/vite.config.js
@@ -11,5 +11,19 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-  ]
+  ],
+  // server: {
+  //   port: '3000',            // 客户端的运行端口，此处也可以绑定vue运行的端口，当然也可以写在pycharm下
+  //   host: 'www.gezixi.online',   // 客户端的运行地址，此处也可以绑定vue运行的域名，当然也可以写在pycharm下
+  //   // 跨域代理
+  //   proxy: {
+  //     '/api': {
+  //       // 凡是遇到 /api 路径的请求，都映射到 target 属性  /api/header/  ---> http://api.gezixi.online:8000/header/
+  //       target: 'http://api.gezixi.online:8000/',
+  //       changeOrigin: true,
+  //       ws: true,    // 是否支持websocket跨域
+  //       rewrite: path => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // },
 })
