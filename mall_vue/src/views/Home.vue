6779cb0031c69fefce574febd1292222dc939ceb
gezixi <1149994515@qq.com>
2022-05-29 02:05:45 +0800

    feature:http请求拦截器配置

--- mall_vue/src/views/Home.vue
+++ mall_vue/src/views/Home.vue
@@ -7,8 +7,17 @@
 </template>
 
 <script setup>
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+  import Header from '../components/Header.vue'
+  import Footer from '../components/Footer.vue'
+  // 测试CORS的跨域配置是否有问题
+
+
+  import http from "../utils/http";
+  // 测试服务端的跨域是否配置成功
+  http.get("http://api.gezixi.online:8000/home/test").then(response=>{
+    console.log(response.data);
+  })
+
 </script>
 
 <style scoped>
