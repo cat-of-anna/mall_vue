3e27a51189dbd18e57009ccb2b0cb719902a8b33
gezixi <1149994515@qq.com>
2022-05-29 00:08:12 +0800

    feature:引入element-plus UI

--- mall_vue/src/views/Home.vue
+++ mall_vue/src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>商城首页</div>
+  <div>
+    <h1>商城首页</h1>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+      <el-button>中文</el-button>
+      <el-rate v-model="store.value2" :colors="store.colors"></el-rate>
+    </el-row>
+  </div>
 </template>
 
 <script setup>
+import {reactive} from "vue";
 
+const store = reactive({
+  value2: null,
+  colors: ['yellow', 'skyblue', 'green']
+})
 </script>
 
 <style scoped>
