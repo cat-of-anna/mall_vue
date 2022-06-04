1dd266ddac9ca001796da0afcadb3cb9b896d981
gezixi <1149994515@qq.com>
2022-06-04 14:52:37 +0800

    feature:路由守卫，防止未登录用户进入需要登录认证的界面

--- mall_vue/src/router/index.js
+++ mall_vue/src/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
+import store from "../store";
 
 
 // 2. 路由列表
@@ -8,6 +9,7 @@ const routes = [
       keepAlive: true
     },
     path: '/',
+    name: "Home",
     component: () => import("../views/Home.vue")
   },
   { meta: {
@@ -15,7 +17,18 @@ const routes = [
       keepAlive: true
     },
     path: '/login',
+    name: "Login",
     component: () => import("../views/Login.vue")
+  },
+  {
+    meta: {
+      title: "在线商城-用户中心",
+      keepAlive: true,
+      authorization: true
+    },
+    path: '/user',
+    name: "User",
+    component: () => import("../views/User.vue")
   },
 ]
 
@@ -24,6 +37,18 @@ const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHashHistory(),
   routes,
+})
+
+// 导航守卫
+router.beforeEach((to, from, next)=>{
+  document.title = to.meta.title;
+  // 如果，没登录
+  if (to.meta.authorization &&!store.getters.getUserInfo) {
+    next({"name": "Login"});
+  }else{
+    // 如果登录了
+    next();
+  }
 })
 
 export default router
