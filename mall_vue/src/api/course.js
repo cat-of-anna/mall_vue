96c6f71ed315044940481b470add68ba770f1eb5
gezixi <1149994515@qq.com>
2022-07-10 03:27:29 +0800

    feature:客户端展示课程方向和分类，分类下拉框显示

---
+++ mall_vue/src/api/course.js
@@ -0,0 +1,19 @@
+import http from "../utils/http";
+import {reactive} from "vue";
+
+
+const course = reactive({
+  current_direction: 0,  // currently, selected direction
+  current_category: 0,  // currently, selected category
+  direction_list: [],  // a list of directions
+  category_list: [],  // a list of categories
+
+  get_course_direction(){
+    return http.get("/project/directions");
+  },
+  get_course_categories(){
+    return http.get("/project/categories");
+  }
+})
+
+export default course;
